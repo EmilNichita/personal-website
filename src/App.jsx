315a3e724ba0dbe72ffb55a7bf9894ff62d4ce8c
EmilNichita <doaremil@gmail.com>
2024-10-24 00:27:58 +0300

    added: skills section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./sections/Hero";
 import Projects from "./sections/Projects";
+import Skills from "./sections/Skills";
 
 function App() {
   return (
     <>
       <Hero />
       <Projects />
+      <Skills />
     </>
   );
 }
