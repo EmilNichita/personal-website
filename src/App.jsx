6273e895c7d6a51b67369b22ccf5a18dc952c768
EmilNichita <doaremil@gmail.com>
2024-10-28 19:23:42 +0200

    added: cursor magic

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./App.css";
 import Hero from "./sections/Hero";
 import Projects from "./sections/Projects";
 import Skills from "./sections/Skills";
+import BinaryTrailCursor from "./common/BinaryTrailCursor";
 
 function App() {
   return (
     <>
+      <BinaryTrailCursor />
       <Hero />
       <Projects />
       <Skills />
